797fcf1940c4946f1bba210186c67b62c6f55caf
mehulhere <99717294+mehulhere@users.noreply.github.com>
2023-05-13 19:15:47 +0530

    Update Assembler_code.py

--- Assembler_code.py
+++ Assembler_code.py
@@ -1,1 +1,29 @@
+opcode_dict = {
+    "add": ["00000", "A"],
+    "sub": ["00001", "A"],
+    "mov_imm": ["00010", "B"],
+    "mov_reg": ["00011", "C"],
+    "ld": ["00100", "D"],
+    "st": ["00101", "D"],
+    "mul": ["00110", "A"],
+    "div": ["00111", "C"],
+    "rs": ["01000", "B"],
+    "ls": ["01001", "B"],
+    "xor": ["01010", "A"],
+    "or": ["01011", "A"],
+    "and": ["01100", "A"],
+    "not": ["01101", "C"],
+    "cmp": ["01110", "C"],
+    "jmp": ["01111", "E"],
+    "jlt": ["11100", "E"],
+    "jgt": ["11101", "E"],
+    "je": ["11111", "E"],
+    "hlt": ["11010", "F"]
+}
+# "A" for arithmetic, "B" for immediate, "C" for register, 
+# "D" for load/store, "E" for jump, and "F" for halt.
+assem_code=open("assem_code.txt")
+open=assem_code.read()
+print(open)
+
 
