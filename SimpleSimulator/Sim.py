9bccba17a085427a0e54ed47e698d6abd86209b8
gautam1228 <112088844+gautam1228@users.noreply.github.com>
2023-06-08 00:01:05 +0530

    Update Sim.py

--- SimpleSimulator/Sim.py
+++ SimpleSimulator/Sim.py
@@ -1,4 +1,7 @@
 import sys
+import time
+
+start_time = time.time()
 instr_dict={
     "00000": "add",
     "00001": "sub",
@@ -16,6 +19,9 @@ instr_dict={
     "01101": "not",
     "01110": "cmp",
     "01111": "jmp",
+    "10000": "addf",
+    "10001": "subf",
+    "10010": "movf",
     "11100": "jlt",
     "11101": "jgt",
     "11111": "je",
@@ -55,6 +61,12 @@ def func_call(instr, inputline):
         func_cmp(inputline)
     elif instr == "jmp":
         func_jmp(inputline)
+    elif instr == "addf":
+        func_addf(inputline)
+    elif instr == "subf":
+        func_subf(inputline)
+    elif instr == "movf":
+        func_subf(inputline)
     elif instr == "jlt":
         func_jlt(inputline)
     elif instr == "jgt":
@@ -79,7 +91,7 @@ def func_add(inputline):
         output_list[8]=0 #reseting FLAGS register to 0
     else:
         output_list[R1]=0
-        output_list[8]+=8
+        output_list[8]=8
 
 def func_sub(inputline):
     # Implementation of the sub instruction
@@ -94,13 +106,13 @@ def func_sub(inputline):
         output_list[8]=0 #reseting FLAGS register to 0
     else:
         output_list[R1]=0
-        output_list[8]+=8
+        output_list[8]=8
 
 def func_movi(inputline):
     # Implementation of the mov instruction
     R1 = int(inputline[1:4],2) + 1
     imm=int(inputline[4:11], 2) 
-    output_list[R1]=imm
+    output_list[R1]=int(imm)
     output_list[8]=0 #reseting FLAGS register to 0
 
 def func_mov(inputline):
@@ -115,7 +127,7 @@ def func_ld(inputline):
     # Implementation of the ld instruction
     R1=int(inputline[1:4],2) + 1
     mem=int(inputline[4:11],2)
-    output_list[R1]=input_list[mem]
+    output_list[R1]=int(input_list[mem],2)
     output_list[8]=0 #reseting FLAGS register to 0
 
 def func_st(inputline):
@@ -138,44 +150,44 @@ def func_mul(inputline):
         output_list[8]=0 #reseting FLAGS register to 0
     else:
         output_list[R1]=0
-        output_list[8]+=8
+        output_list[8]=8
 
 def func_div(inputline):
     # Implementation of the div instruction
     R1 = int(inputline[5:8],2) + 1
     R2 = int(inputline[8:11],2) + 1
-    if R2 != 0:
-        quotient=R1/R2
-        remainder=R1%R2
+    num1=output_list[R1]
+    num2=output_list[R2]
+    if num2 != 0:
+        quotient=int(num1//num2)
+        remainder=int(num1%num2)
         output_list[1]=quotient
         output_list[2]=remainder
         output_list[8]=0 #reseting FLAGS register to 0
     else:
-        output_list[8]+=8
+        output_list[8]=8
         output_list[1]=0
         output_list[2]=0
 
 def func_rs(inputline):
     # Implementation of the rs instruction
-    R1 = int(inputline[1:4],2) + 1
-    imm=int(inputline[4:11], 2) 
-    num=output_list[R1]
-    while(imm):
-        num=int(num/2)
-        imm-=1
-    output_list[R1]=num
-    output_list[8]=0 #reseting FLAGS register to 0
+    R1 = int(inputline[1:4], 2) + 1
+    imm = int(inputline[4:11], 2)
+    num = output_list[R1]
+    num = num >> imm
+    output_list[R1] = num
+    output_list[8] = 0  # resetting FLAGS register to 0
+
 
 def func_ls(inputline):
     # Implementation of the ls instruction
-    R1 = int(inputline[1:4],2) + 1
-    imm=int(inputline[4:11], 2) 
-    num=output_list[R1]
-    while(imm):
-        num=int(num*2)
-        imm-=1
-    output_list[R1]=num
-    output_list[8]=0 #reseting FLAGS register to 0
+    R1 = int(inputline[1:4], 2) + 1
+    imm = int(inputline[4:11], 2)
+    num = output_list[R1]
+    num = num << imm
+    output_list[R1] = num
+    output_list[8] = 0  # resetting FLAGS register to 0
+
 
 def func_xor(inputline):
     # Implementation of the xor instruction
@@ -215,7 +227,7 @@ def func_not(inputline):
     R1 = int(inputline[5:8],2) + 1
     R2 = int(inputline[8:11],2) + 1
     num=output_list[R2]
-    output_list[R1]=~num
+    output_list[R1]=2**16-num-1
     output_list[8]=0 #reseting FLAGS register to 0
     
 
@@ -226,26 +238,32 @@ def func_cmp(inputline):
     num1=output_list[R1]
     num2=output_list[R2]  
     output_list[8]=0
+    # print(num1,num2)
+    # print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     if num1<num2:
-        output_list[8]+=4
+        output_list[8]=4
     elif num1>num2:
-        output_list[8]+=2
+        output_list[8]=2
     else:
-        output_list[8]+=1 
+        # print("ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddadadjasdc asjdc ")
+        output_list[8]=1 
 
 def func_jmp(inputline):
     # Implementation of the jmp instruction
     mem=int(inputline[4:11],2) 
     global pc
-    pc=mem
+    pc=int(mem)
     output_list[8]=0
     
+
+    
+
 def func_jlt(inputline):
     # Implementation of the jlt instruction
     if output_list[8]==4:
         mem=int(inputline[4:11],2) 
         global pc
-        pc=mem
+        pc=int(mem)
     output_list[8]=0
 
 def func_jgt(inputline):
@@ -253,21 +271,22 @@ def func_jgt(inputline):
     if output_list[8]==2:
         mem=int(inputline[4:11],2) 
         global pc
-        pc=mem
+        pc=int(mem)
     output_list[8]=0
 
 def func_je(inputline):
     # Implementation of the je instruction
     if output_list[8]==1:
+        
         mem=int(inputline[4:11],2) 
         global pc
-        pc=mem
+        pc=int(mem)
     output_list[8]=0
     
 def func_hlt(inputline):
     # Implementation of the hlt instruction
     global pc
-    pc=256
+    pc=128
     output_list[8]=0
 
     
@@ -279,11 +298,13 @@ for line in sys.stdin:
         break
     input_list.append(line)
 while(len(input_list) < 128):
-    input_list.append(0)
+    input_list.append("0"*16)
 pc=0
 while(pc<128):
+    elapsed_time = time.time() - start_time
+    if elapsed_time > 2:
+        break
     line=input_list[pc]
-    # print(pc)
     if line !=0:
         line=str(line)
         opcode=line[0:5]
@@ -296,7 +317,9 @@ while(pc<128):
             elif i==8:
                 print(str(bin(output_list[i]))[2:].zfill(16))
             else:
+                # print([i for i in output_list if type(i)==float])
+                # print(output_list)
                 print(str(bin(output_list[i]))[2:].zfill(16), end=" ")
-        output_list[0]=pc
+        output_list[0]=int(pc)
 for i in input_list:
     print(str(i).zfill(16))
